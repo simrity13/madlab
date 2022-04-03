6bd7f888292e05f6d7e0883deba1931ae8a7b458
Simrity <simritybhat@gmail.com>
2022-04-03 16:27:42 +0530

    Updates

---
+++ precache-manifest.4df1f1139db1968396edf260525b7a90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebf6e5ccb59f40a9721c7ced6284d820",
+    "url": "/madlab/index.html"
+  },
+  {
+    "revision": "467f85b604a3f1524c26",
+    "url": "/madlab/static/css/2.9be35272.chunk.css"
+  },
+  {
+    "revision": "6e0720a8fab9dc11c66a",
+    "url": "/madlab/static/css/main.e0e55565.chunk.css"
+  },
+  {
+    "revision": "467f85b604a3f1524c26",
+    "url": "/madlab/static/js/2.903e3fb3.chunk.js"
+  },
+  {
+    "revision": "6e0720a8fab9dc11c66a",
+    "url": "/madlab/static/js/main.c0749926.chunk.js"
+  },
+  {
+    "revision": "396f417b8273cab40f0e",
+    "url": "/madlab/static/js/runtime~main.05b0808b.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/madlab/static/media/logo.9f7e506d.svg"
+  }
+]);
